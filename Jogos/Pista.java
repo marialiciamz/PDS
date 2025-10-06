e43dea3afe6950cb9aa59c9cc3671f1edbce6a71
Luís Antônio <luis.antonio1@escolar.ifrn.edu.br>
2025-10-06 11:52:19 -0300

    comentando a classe pista

--- Jogos/Pista.java
+++ Jogos/Pista.java
@@ -1,23 +1,32 @@
 
+// Classe que representa uma pista de corrida
 public class Pista {
 
+    // Atributo que armazena o tipo da pista (ex: "asfalto", "terra", "neve")
     private String tipo;
-       private int dificuldade;
 
-       public Pista(String tipo, int dificuldade) {
-           this.tipo = tipo;
-           this.dificuldade = dificuldade;
-       }
+    // Atributo que armazena o nível de dificuldade da pista (ex: 1 a 10)
+    private int dificuldade;
 
-       public void exibirDetalhes() {
-           System.out.println("Pista: " + tipo + " | Dificuldade: " + dificuldade);
-       }
+    // Construtor da classe Pista, que inicializa o tipo e a dificuldade
+    public Pista(String tipo, int dificuldade) {
+        this.tipo = tipo;
+        this.dificuldade = dificuldade;
+    }
 
-       public boolean pistaMolhada(boolean chovendo) {
-           return chovendo;
-       }
+    // Método que exibe os detalhes da pista no console
+    public void exibirDetalhes() {
+        System.out.println("Pista: " + tipo + " | Dificuldade: " + dificuldade);
+    }
 
-       public void ajustarDificuldade(int novaDificuldade) {
-           this.dificuldade = novaDificuldade;
-       }
-   }
+    // Método que verifica se a pista está molhada, dependendo da condição de chuva
+    // Retorna verdadeiro se estiver chovendo, falso caso contrário
+    public boolean pistaMolhada(boolean chovendo) {
+        return chovendo;
+    }
+
+    // Método que permite ajustar o nível de dificuldade da pista
+    public void ajustarDificuldade(int novaDificuldade) {
+        this.dificuldade = novaDificuldade;
+    }
+}
